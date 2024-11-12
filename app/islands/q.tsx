5c5a10f7afe3cc31ae313e9e48287651946f2ebc
ikoamu <shotaro.aoki@esm.co.jp>
2024-11-12 18:33:58 +0900

    イントロクイズができるようにした

--- app/islands/q.tsx
+++ app/islands/q.tsx
@@ -1,17 +1,84 @@
-import { useState } from 'hono/jsx'
+import { useEffect, useRef, useState } from 'hono/jsx'
 import type { Quiz } from '../quiz'
 
-export default function Q(props: { quizList: Quiz[] }) {
-  const [index, setIndex] = useState(0)
-  const quiz = props.quizList[index]
+type QuizStatus = {
+  quizIndex: number;
+  correctCount: number;
+  status: 'ready' | 'playing' | "answer" | 'gameover' | 'gameclear';
+}
+
+const initialStatus: QuizStatus = {
+  quizIndex: 0,
+  correctCount: 0,
+  status: 'ready',
+}
+
+export default function Q({quizList}: { quizList: Quiz[] }) {
+  const [status, setStatus] = useState(initialStatus)
+  // const volumeRef = useRef<HTMLInputElement>(null)
+  // const audioRef = useRef<HTMLAudioElement>(null)
+  const quiz = quizList[status.quizIndex]
+  const [count, setCount] = useState(0)
+
+
+  // useEffect(() => {
+  //   if (audioRef.current) {
+  //     audioRef.current.volume = 0.1;
+  //   }
+  // }, [])
+
+
   return (
     <div>
-      <h2>Question {index + 1}</h2>
-      <audio controls src={quiz.answer.preview_url} />
-      {quiz.options.map((option) => (
-        <button>{option.text}</button>
-      ))}
-      {JSON.stringify(quiz, null, 2)}
+        {status.status === 'ready' && (
+        <button onClick={() => setStatus({ ...status, status: 'playing' })}>Start</button>
+      )}
+      {status.status === 'playing' && (<>
+        <h1>Q. {status.quizIndex + 1}</h1>
+        <audio src={quiz.answer.preview_url} controls />
+        <ul>
+          {quiz.options.map((choice, index) => (
+            <li key={index}>
+              <button onClick={() => {
+                if (choice.isCorrect) {
+                  setStatus({
+                    ...status,
+                    correctCount: status.correctCount + 1,
+                    status: status.quizIndex + 1 === quizList.length ? 'gameclear' : 'answer',
+                  })
+                } else {
+                  setStatus({
+                    ...status,
+                    status: 'gameover',
+                  })
+                }
+              }}>{choice.text}</button>
+            </li>
+          ))}
+        </ul>
+        </>
+      )}
+      {
+        status.status === 'answer' && (
+          <div>
+            <p>Correct!</p>
+            <button onClick={() => setStatus({ ...status, status: 'playing', quizIndex: status.quizIndex + 1 })}>Next</button>
+          </div>
+        )
+      }
+      {status.status === 'gameover' && (
+        <div>
+          <p>Game Over</p>
+          <p>{status.correctCount} / {quizList.length}</p>
+          <button onClick={() => setStatus(initialStatus)}>Retry</button>
+        </div>
+      )}
+      {status.status === 'gameclear' && (
+        <div>
+          <p>Correct: {status.correctCount}</p>
+          <button onClick={() => setStatus(initialStatus)}>Retry</button>
+        </div>
+      )}
     </div>
   )
 }
